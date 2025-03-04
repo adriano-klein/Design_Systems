200e7542601584552ba3162f3a585e6f197bf3e2
adriano25klein@gmail.com <you@example.com>
2025-03-04 10:40:49 -0300

    Commit inicial

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,8 @@
+import { styled } from './styles/index.js'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  height: '$8',
+})
