aaa7a3dddd16514816abab2878cc12feddbe2435
adriano25klein@gmail.com <you@example.com>
2025-03-16 08:04:44 -0300

    fix: inseri o subpath para producao do storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,40 +1,46 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import { join, dirname } from 'path';
+import type { StorybookConfig } from "@storybook/react-vite";
+import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
+  return dirname(require.resolve(join(value, "package.json")));
 }
 
 const config: StorybookConfig = {
   stories: [
-    '../src/pages/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    "../src/pages/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
   addons: [
     {
-      name: getAbsolutePath('@storybook/addon-essentials'),
+      name: getAbsolutePath("@storybook/addon-essentials"),
       options: {
         docs: true,
       },
     },
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {
     autodocs: true,
   },
   typescript: {
-    reactDocgen: 'react-docgen-typescript',
+    reactDocgen: "react-docgen-typescript",
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/Design_Systems/";
+    }
+    return config;
   },
 };
 
